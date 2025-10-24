837329bdb2d6942e7ce0108ff16e7094b95f456c
Dravenox <satandungeon1@gmail.com>
2025-10-24 17:35:42 +0700

    Create SatanLoader.lua

---
+++ SatanLoader.lua
@@ -0,0 +1,17 @@
+function log(str: string)
+		pcall(function()
+				game.StarterGui:SetCore('SendNotification', {
+						Title = 'Satan Script',
+						Text = str,
+						Icon = 'rbxassetid://109030636181557',
+						Duration = 4
+				})
+		end)
+end
+
+if game.PlaceId == 121864768012064 then
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/dravenox/roblox/refs/heads/main/SatanNotHub.lua'))()
+		log('Working!')
+else
+		log('Game Not Supported!')
+end
